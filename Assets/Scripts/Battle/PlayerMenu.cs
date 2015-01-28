c1910f5746fca941f4bb65bc1e697a9192ab6b24
SamuraiSusy <susyy-@hotmail.com>
2015-01-28 13:36:28 +0200

    playermenu started

---
+++ Assets/Scripts/Battle/PlayerMenu.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMenu : MonoBehaviour
+{
+    public GameObject StateMachine;
+    public GameObject menuPref, buttonPref;
+
+	// Use this for initialization
+	void Start ()
+    {
+        StateMachine = GameObject.FindWithTag("StateMachine");
+        StateMachine statemachine = StateMachine.GetComponent<StateMachine>();
+
+            Debug.Log("in playermenu()");
+             //8
+
+            Instantiate(menuPref, new Vector3(-4, -3.5f), Quaternion.identity);
+            for (int i = 0; i < 4; i++)
+            {
+                if (i < 2)
+                {
+                    float offSet1 = 0;
+                    Instantiate(buttonPref, new Vector3(-5 + offSet1, -1), Quaternion.identity);
+                    offSet1 += 3;
+                }
+                else if (i >= 2 && i < 4)
+                {
+                    float offSet1 = -3;
+                    float offSet2 = 0;
+
+                    Instantiate(buttonPref, new Vector3(-5 + offSet1, -1 + offSet2), Quaternion.identity);
+                    offSet2 -= -3;
+                }
+            }
+
+        Debug.Log("playermenu start()");
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	    
+	}
+}
---
+++ Assets/Scripts/Battle/StateMachine.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StateMachine : MonoBehaviour
+{
+
+    public string[] battleStates = new string[] {  
+        "Start",
+        "PlayerMenu",
+        "PlayerTurn",
+        "EnemyTurn",
+        "Win",
+        "Lose",
+        "Run"
+                                                };
+    public string currentState;
+
+	// Use this for initialization
+	void Start ()
+    {
+        currentState = "Start";
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (currentState == "Start")
+            currentState = "PlayerMenu";
+        else if(currentState == "PlayerMenu")
+        {
+        }
+	}
+}
